9f9104fd6519d0d93d847eaf55fe6af7b4547d5e
Eric Helin <ehelin@felpsis.net>
2019-01-07 21:15:59 -0600

    conditionally display login screen or main panel

--- TgimbaNetCoreWebReactJs/ClientApp/src/components/Login.js
+++ TgimbaNetCoreWebReactJs/ClientApp/src/components/Login.js
@@ -17,60 +17,67 @@ class Login extends React.Component {
 		}  			
 	}
 
-	render() {								
-		let { username, password } = this.state;	
-
-		const processLogin = _ => {									 
-			this.props.login(username, password);
-		}					
-
-		const navigateRegistration = _ => {
-			var utils = Object.create(utilsRef.Utilities);
+	render() {
+		var utils = Object.create(utilsRef.Utilities);
+		if (utils.IsLoggedIn()) {
 			var host = utils.GetHost();
-			window.location = host + '/register';
+			window.location = host + '/main';
 		}
+		else {
+			let { username, password } = this.state;
 
-		var tableStyle = {					 
-			"width": "100%",							 
-			"text-align": "center",
-			"vertical-align":" middle"
-		};
+			const processLogin = _ => {
+				this.props.login(username, password);
+			}
 
-		return (		   
-			<div>
-				<h1>React JS - Login</h1>
-				<table style={tableStyle}>
-					<tr>
-						<td>
-							<label>Username:</label>
-							<input
-								id="USER_CONTROL_LOGIN_USERNAME"
-								type="text"
-								value={username}
-								onChange={event => this.setState({ username: event.target.value })}
-							/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label>Password:</label>
-							<input
-								id="USER_CONTROL_LOGIN_PASSWORD"
-								type="password"
-								value={password}
-								onChange={event => this.setState({ password: event.target.value })}
-							/> 
-						</td>
-					</tr>
-					<tr>
-						<td>		 								   
-							<Button onPress={processLogin} id="hvJsLoginBtn">Login</Button>
-							<Button onPress={navigateRegistration} id="hvJsRegisterPanelBtn">Register</Button>							  
-						</td>
-					</tr>
-				</table>
-			</div>
-		);
+			const navigateRegistration = _ => {
+				var utils = Object.create(utilsRef.Utilities);
+				var host = utils.GetHost();
+				window.location = host + '/register';
+			}
+
+			var tableStyle = {
+				"width": "100%",
+				"text-align": "center",
+				"vertical-align": " middle"
+			};
+
+			return (
+				<div>
+					<h1>React JS - Login</h1>
+					<table style={tableStyle}>
+						<tr>
+							<td>
+								<label>Username:</label>
+								<input
+									id="USER_CONTROL_LOGIN_USERNAME"
+									type="text"
+									value={username}
+									onChange={event => this.setState({ username: event.target.value })}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label>Password:</label>
+								<input
+									id="USER_CONTROL_LOGIN_PASSWORD"
+									type="password"
+									value={password}
+									onChange={event => this.setState({ password: event.target.value })}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<Button onPress={processLogin} id="hvJsLoginBtn">Login</Button>
+								<Button onPress={navigateRegistration} id="hvJsRegisterPanelBtn">Register</Button>
+							</td>
+						</tr>
+					</table>
+				</div>
+			);
+		}
 	};
 }
 
