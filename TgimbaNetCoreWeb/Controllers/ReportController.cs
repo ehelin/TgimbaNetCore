34fa865162e60c3315f7beecbc02ed3bc5b8fbd6
Eric Helin <ehelin@felpsis.net>
2019-04-28 00:06:42 -0500

    temp disable of report display

--- TgimbaNetCoreWeb/Controllers/ReportController.cs
+++ TgimbaNetCoreWeb/Controllers/ReportController.cs
@@ -22,39 +22,39 @@ namespace TgimbaNetCoreWeb.Controllers
         [HttpPost]
         public IActionResult Login(ReportIndexModel model)
         {
-            if(model != null)
-            {
-                if (!string.IsNullOrEmpty(model.UserName) 
-                        && !string.IsNullOrEmpty(model.Password)
-                            && model.UserName == Shared.Credentials.GetReportUser()
-                                && model.Password == Shared.Credentials.GetReportPassword())
-                {
-                    HttpContext.Session.SetString("ReportToken", Shared.Credentials.GetReportToken());
-                    return RedirectToAction("ReportDisplay");
-                }                
-            }
+            //if(model != null)
+            //{
+            //    if (!string.IsNullOrEmpty(model.UserName) 
+            //            && !string.IsNullOrEmpty(model.Password)
+            //                && model.UserName == Shared.Credentials.GetReportUser()
+            //                    && model.Password == Shared.Credentials.GetReportPassword())
+            //    {
+            //        HttpContext.Session.SetString("ReportToken", Shared.Credentials.GetReportToken());
+            //        return RedirectToAction("ReportDisplay");
+            //    }                
+            //}
 
-            if (model == null)
-            {
-                model = new ReportIndexModel();
-            }
+            //if (model == null)
+            //{
+            //    model = new ReportIndexModel();
+            //}
 
-            model.Error = "Login Failed";
+            //model.Error = "Login Failed";
 
             return View("Index", model);
         }
 
         public IActionResult ReportDisplay()
         {
-            var token = HttpContext.Session.GetString("ReportToken");
-            if (string.IsNullOrEmpty(token) || token != Shared.Credentials.GetReportToken())
-            {
-                return RedirectToAction("Index");
-            }
+            //var token = HttpContext.Session.GetString("ReportToken");
+            //if (string.IsNullOrEmpty(token) || token != Shared.Credentials.GetReportToken())
+            //{
+            //    return RedirectToAction("Index");
+            //}
 
             var model = new ReportDisplayModel();
 
-            model.Report = this.service.GetReport();
+            //model.Report = this.service.GetReport();
 
             return View(model);
         }
