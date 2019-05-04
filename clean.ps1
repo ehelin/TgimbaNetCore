55f85578cea753b39cdf6b476b0170ec15bae051
Eric Helin <ehelin@felpsis.net>
2019-05-03 23:14:14 -0500

    continued working on power shell script to clean remote ftp directory prior to pipeline deploying...getting closer to working

--- clean.ps1
+++ clean.ps1
@@ -1,3 +1,45 @@
+
+Function DeleteFile {
+Param
+(
+ [string]$username,
+ [string]$password,
+ [string]$fileDirectory,
+ [string]$uri,
+ [string]$isFile
+)
+	write-host "DeleteFile(args)"
+
+    $deletePath = "";
+    
+    if ($isFile -eq "true") 
+    {
+        $deletePath = "$uri/$fileDirectory"
+    } 
+    else 
+    {
+        $deletePath = "$uri"
+    }
+	write-host "delete path: $deletePath"
+    
+    $ftprequest = [System.Net.FtpWebRequest]::create($deletePath)
+    $ftprequest.Credentials =  New-Object System.Net.NetworkCredential($username, $password)
+
+    try
+    {
+        if ($isFile -eq "true") {
+            $ftprequest.Method = [System.Net.WebRequestMethods+Ftp]::DeleteFile
+        } else {
+            $ftprequest.Method = [System.Net.WebRequestMethods+Ftp]::RemoveDirectory
+        }
+        $ftprequest.GetResponse() | Out-Null
+    }
+    catch
+    {
+		Write-Host "DeleteFile(args)-Error: "$_.Exception.Message
+    }
+}
+
 Function CleanFtpSite { 
 Param 
 (
@@ -6,98 +48,65 @@ Param
  [string]$password,
  [string]$directory
 )
+    try 
+    { 
+	    write-host "CleanFtpSite(args)"
+        $uri =  "$server$directory" 
 
-try 
- {
-    $uri =  "$server$directory" 
-    write-host "url: " + $uri
-
-	# TODO - refactor to another method
-    $FTPRequest = [System.Net.FtpWebRequest]::Create($uri)
-    $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($username, $password)
-    $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
-    $FTPResponse = $FTPRequest.GetResponse() 
-    $ResponseStream = $FTPResponse.GetResponseStream()
+        write-host ""
+        write-host "Getting files ======================================================"
+        write-host "uri: uri"
+  
+        $FTPRequest = [System.Net.FtpWebRequest]::Create($uri)
+        $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($username, $password)
+        $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
+        $FTPResponse = $FTPRequest.GetResponse() 
+        $ResponseStream = $FTPResponse.GetResponseStream()
         
-    $StreamReader = New-Object System.IO.StreamReader $ResponseStream  
-    $files = New-Object System.Collections.ArrayList
-    While ($fileDirectory = $StreamReader.ReadLine())
-    {
-        write-host "FileDirectory: " + $fileDirectory
-        $files.Add($fileDirectory);
-    }
+        $StreamReader = New-Object System.IO.StreamReader $ResponseStream  
+        $files = New-Object System.Collections.ArrayList
+        While ($fileDirectory = $StreamReader.ReadLine())
+        {
+            write-host "FileDirectory: " + $fileDirectory
+            $files.Add($fileDirectory);
+        }
         
-    $StreamReader.close()
-    $ResponseStream.close()
-    $FTPResponse.Close()
-
-    Foreach ($fileDirectory in $files) 
-    {
-        $pos = $fileDirectory.IndexOf(".")
-        $isFile = "false"
-
-        # its a directory
-        if ($fileDirectory.IndexOf(".") -le 0) 
+        $StreamReader.close()
+        $ResponseStream.close()
+        $FTPResponse.Close()
+        
+        write-host ""
+        write-host "Processing files ======================================================"
+        Foreach ($fileDirectory in $files) 
         {
-            write-host "directory: " + $fileDirectory
-            CleanFtpSite -server $server -username $username -password $password -directory "$directory/$fileDirectory"
-        }      
-        # its a file
-        else 
-        {        
-            $isFile = "true"
-            write-host deleteing $fileDirectory
+            write-host $fileDirectory
+      
+            $pos = $fileDirectory.IndexOf(".")
+            $isFile = "false"
 
-            # start delete file =========================================================================
-			# TODO - refactor
-            $deleteFile = "$uri/$fileDirectory"
-            $ftprequest = [System.Net.FtpWebRequest]::create($deleteFile)
-            $ftprequest.Credentials =  New-Object System.Net.NetworkCredential($username, $password)
-
-            try
+            # its a directory
+            if ($fileDirectory.IndexOf(".") -le 0) 
             {
-               $ftprequest.Method = [System.Net.WebRequestMethods+Ftp]::DeleteFile
-               $ftprequest.GetResponse() | Out-Null
-            }
-            catch
-            {
-				Write-Host $_.Exception.Message
-            }
-            # end delete file =========================================================================
-        }
-    }
+                write-host "directory: " + $fileDirectory
+                CleanFtpSite -server $server -username $username -password $password -directory "$directory/$fileDirectory"
+            }      
+            # its a file
+            else 
+            {        
+                $isFile = "true"
 
-    if ($isFile -eq "true")
+                write-host "file: " + $fileDirectory
+                DeleteFile -username $username -password $password -fileDirectory $fileDirectory -uri $uri -isFile $isFile
+            }           
+        }         
+
+        write-host "clean directory: " + $uri
+        DeleteFile -username $username -password $password -fileDirectory $fileDirectory -uri $uri -isFile "false"
+    }
+    catch 
     {
-        #start delete directory containing files just deleted ==============================
-		# TODO - refactor
-        $ftprequest = [System.Net.FtpWebRequest]::create($uri)
-        $ftprequest.Credentials =  New-Object System.Net.NetworkCredential($username, $password)
-
-        try
-        {
-            $ftprequest.Method = [System.Net.WebRequestMethods+Ftp]::RemoveDirectory
-            $ftprequest.GetResponse() | Out-Null
-        }
-        catch
-        {
-			Write-Host $_.Exception.Message
-        }
+		Write-Host "CleanFtpSite(args)-Error: "$_.Exception.Message
     }
-    #end delete directory containing files just deleted ================================
-
-}
-catch {
-    write-host -message $_.Exception.InnerException.Message
-}
-
-    $StreamReader.close()
-    $ResponseStream.close()
-    $FTPResponse.Close()
-
-    Return $files
-
-
 }
 
 $server = ''
@@ -107,4 +116,4 @@ $directory =''
 
 CleanFtpSite -server $server -username $username -password $password -directory $directory
 
-Write-Output $filelist
+write-host "Done!"
