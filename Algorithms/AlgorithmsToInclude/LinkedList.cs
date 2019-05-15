3b2986337eb52d2f2c11402040428ce20f92bcaa
Eric Helin <ehelin@felpsis.net>
2019-05-14 21:38:05 -0500

    Adding a shell to implement (one day)

---
+++ Algorithms/AlgorithmsToInclude/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.AlgorithmsToInclude
+{
+    // TODO - implement this single linked list and use for something (perhaps your own stack)
+    public class List
+    {
+        public LinkedList Head { get; set; }
+        public LinkedList Tail { get; set; }
+
+        public void Append(LinkedList linkedList, int Value) 
+        {
+            throw new NotImplementedException();
+        }
+        public void PrePend(LinkedList linkedList, int value)
+        {
+            throw new NotImplementedException();
+        }
+        public int Length() 
+        {
+            throw new NotImplementedException();
+        }
+        public LinkedList NodeAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+        public LinkedList NodeAtEnd(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class LinkedList
+    {
+        public int Value { get; set; }
+        public LinkedList Next { get; set; }
+    }
+}
