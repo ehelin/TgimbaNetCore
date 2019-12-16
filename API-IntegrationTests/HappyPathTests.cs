d1c36034a535f3033f4c56ed4954ae4c12efff00
Eric Helin <ehelin@felpsis.net>
2019-12-15 21:22:19 -0600

    added registration step to integration test

--- API-IntegrationTests/HappyPathTests.cs
+++ API-IntegrationTests/HappyPathTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Shared.dto.api;
 
 namespace API_IntegrationTests
 {
@@ -13,6 +15,7 @@ namespace API_IntegrationTests
         public void HappyPathTest()
         {
             EndPoint_TestPage();
+            EndPoint_Register();
         }
 
         private void EndPoint_TestPage()
@@ -22,15 +25,70 @@ namespace API_IntegrationTests
 
             Assert.AreEqual("Test Service Response", result);
         }
+        private void EndPoint_Register()
+        {            
+            var request = new RegistrationRequest() 
+            {
+                Login = new LoginRequest() 
+                { 
+                    EncodedUserName = Base64Encode("fredFlintstone"),
+                    EncodedPassword = Base64Encode("wilmaRules87&")
+                },
+                EncodedEmail = Base64Encode("fred@bedrock.com")
+            };
+            
+            var json = JsonConvert.SerializeObject(request);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var url = host + "/api/tgimbaapi/processuserregistration";
+            var result = Post(url, content).Result;
+
+            Assert.AreEqual(true, System.Convert.ToBoolean(result));
+        }
+
+        #region Http methods
+
+        private async Task<string> Post(string url, StringContent content)
+        {
+            var client = new HttpClient();
+
+            var response = await client.PostAsync(url, content);
+            CheckStatus(response);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return result;
+        }
 
         private async Task<string> Get(string url)
         {
             var client = new HttpClient();
 
             var response = await client.GetAsync(url);
+            CheckStatus(response);
+
             var result = await response.Content.ReadAsStringAsync();
 
             return result;
         }
+
+        #endregion
+
+        #region Private methods
+
+        private void CheckStatus(HttpResponseMessage response)
+        {
+            Assert.IsNotNull(response);
+            
+            //Assume everything completes ok
+            Assert.AreEqual(200, (int)response.StatusCode);  
+        }
+
+        private string Base64Encode(string value)
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(value);
+            return System.Convert.ToBase64String(data);
+        }
+
+        #endregion
     }
 }
