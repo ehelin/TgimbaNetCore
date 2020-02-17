ee271bdedbd7a0f558d1fbc10029d369df289ca2
Eric Helin <ehelin@felpsis.net>
2020-02-16 21:44:31 -0600

    adding local config

---
+++ TestShared/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.misc
+{
+    public class Enums
+    {
+        public enum LogLevel { Error, Info, Verbose }  
+        public enum BucketListItemTypes { Cool, Cold, Warm, Hot }
+        public enum SortColumns 
+        { 
+            ListItemName, 
+            Created, 
+            Category, 
+            Achieved 
+        }
+        public enum SortAlgorithms
+        {
+            Linq,
+            Bubble
+        }
+    }
+}
