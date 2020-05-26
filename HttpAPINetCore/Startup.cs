b61a1fbe8e75d1e59952690e1323335b9b1c5a5d
Eric Helin <ehelin@felpsis.net>
2020-05-25 20:50:25 -0500

    removing using statement

--- HttpAPINetCore/Startup.cs
+++ HttpAPINetCore/Startup.cs
@@ -17,7 +17,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shared.interfaces;
 using Swashbuckle.AspNetCore.Swagger;
-//using TgimbaNetCoreWebShared;
 
 namespace HttpAPINetCore
 {
