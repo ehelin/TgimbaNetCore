5cad006919455f61c8190b65e6f846bad8552ba3
Eric Helin <ehelin@felpsis.net>
2020-06-12 22:37:56 -0500

    compiling :)

--- TgimbaNetCoreWebAngular6/Startup.cs
+++ TgimbaNetCoreWebAngular6/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using TgimbaNetCoreWebShared;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TgimbaNetCoreWebShared;
 
 namespace TgimbaNetCoreWeb
 {
